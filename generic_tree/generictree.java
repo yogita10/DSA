fc40e295dc108fa950e39a1c7873fb46394aa0f6
Yogita Khurana <54748438+yogita10@users.noreply.github.com>
2021-02-14 22:18:29 +0530

    Add files via upload

---
+++ generic_tree/generictree.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class generictree{
+
+
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();   
+        int[] arr=new int[n];     
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+    }
+}
